00f443ed2943e004b4256de5307d7ee229bde33a
naazneen-jatu <naazneen@aiolos.solutions>
2022-01-28 19:33:23 +0530

    submission

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.8.4;
 
 contract Example2 {
     uint public counter;
- 
-    function incrementBy(uint[] memory arr) public {
-        for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+
+    function incrementBy(uint[] calldata arr) public {
+        uint _counter;
+        for (uint idx = 0; idx < arr.length; idx++) {
+            _counter += arr[idx];
         }
+        counter = _counter;
     }
 }
